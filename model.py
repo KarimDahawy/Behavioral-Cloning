12cd44ac869b764d410e393e0fa88b9ef3c3c42a
Karim DAHAWY <karim.dahawy@valeo.com>
2018-01-04 00:50:53 +0200

    Update Model

--- model.py
+++ model.py
@@ -145,12 +145,3 @@ history_object = model.fit_generator(train_generator, samples_per_epoch= \
 model.save('model.h5')
 print("Model Saved")
 print()
-
-# plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
